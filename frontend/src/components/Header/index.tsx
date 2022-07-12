6021d6f7b5a6a6d9e3c723fb99b838657a9953cb
DevLeandro305 <lfsoares.dev@gmail.com>
2022-07-12 11:51:33 -0300

    Use state

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
                 <img src={logo} alt="DSMeta" />
                 <h1>DSMeta</h1>
                 <p>
-                    Desenvolvido por 
+                    Desenvolvido por @
                     <a href="https://www.linkedin.com/in/leandro-ferreira-soares-73500b5a/">Leandro Soares</a>
                 </p>
             </div>
