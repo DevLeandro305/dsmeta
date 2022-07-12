f00b9859ea1bfc857b625716e3b8ebcbd98e71ea
DevLeandro305 <lfsoares.dev@gmail.com>
2022-07-12 11:31:38 -0300

    Date Picker

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,8 +9,8 @@ function Header() {
                 <img src={logo} alt="DSMeta" />
                 <h1>DSMeta</h1>
                 <p>
-                    Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">Leandro Soares</a>
+                    Desenvolvido por 
+                    <a href="https://www.linkedin.com/in/leandro-ferreira-soares-73500b5a/">Leandro Soares</a>
                 </p>
             </div>
         </header>
